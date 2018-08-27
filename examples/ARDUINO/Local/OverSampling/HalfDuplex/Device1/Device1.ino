8934f4a3cc771513d9a4ac8719a0f01a504496af
Raymond <rdsweet@yahoo.com>
2018-08-27 09:56:20 -0500

    language check
    please check this. I think it's changing things for others and not just me. I think I will start doing branches again.

--- examples/ARDUINO/Local/OverSampling/HalfDuplex/Device1/Device1.ino
+++ examples/ARDUINO/Local/OverSampling/HalfDuplex/Device1/Device1.ino
@@ -21,7 +21,7 @@ void setup() {
 };
 
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
-  /* Make use of the payload before sending something, the buffer where payload points to is
+  /* Make use of the payload before sending something. The buffer where payload points to is
      overwritten when a new message is dispatched */
   if((char)payload[0] == 'B') {
     bus.reply("B", 1);
